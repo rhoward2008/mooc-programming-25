4ea857a6cbc4f0ca17797d667dfbcef8f4bb8bbc
Dworkin <bension.dworkin@baesystems.us>
2025-05-29 19:12:36 -0700

    update 5-29-25

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def longest(strings:list) -> str:
+    '''This function takes a list of strings as input and returns the longest one in the list'''
+    
+    #initialize to the first value in the list
+    longest = strings[0]
+
+    for i in strings:
+        if len(i) > len(longest):
+            longest = i
+    
+    return longest
+
+if __name__ == "__main__":
+    #strings = ["hi", "hiya", "hello", "howdydoody", "hi there","sdfffffffffffffffffffff"]
+    strings = ["sdfffffffffffffffffffff", "hiya", "hello", "howdydoody", "hi there",""]
+    print(longest(strings))
