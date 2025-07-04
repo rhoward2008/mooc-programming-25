29c909175c6260c5aec242242f72dc0aa224acd9
rhoward2008 <ryanhoward2008@yahoo.com>
2025-07-03 17:03:59 -0700

    tuple

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def create_tuple(x: int, y: int, z: int) -> tuple:
+    sorted_list = sorted([x,y,z])
+    return sorted_list[0], sorted_list[2], x+y+z 
+
+if __name__ == '__main__':
+    print(create_tuple(5,3,-1))
--- part05-24_oldest_person/src/oldest_person.py
+++ part05-24_oldest_person/src/oldest_person.py
@@ -1,1 +1,18 @@
 # Write your solution here
+def oldest_person(people:list) -> str:
+    age = 9999
+    name = ''
+    for person in people:
+        if person[1] < age:
+            age = person[1]
+            name = person[0]
+    
+    return name
+
+if __name__ == '__main__':
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+    print(oldest_person(people))
