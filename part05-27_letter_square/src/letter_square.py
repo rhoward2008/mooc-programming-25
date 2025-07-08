5a2c72a02cebd17b6315b6d55f21b7bb3e545567
Dworkin <bension.dworkin@baesystems.us>
2025-07-08 15:11:05 -0700

    last problem

--- part05-27_letter_square/src/letter_square.py
+++ part05-27_letter_square/src/letter_square.py
@@ -7,15 +7,21 @@ count = int(input('Layers:'))
 
 alphabet = alphabet[0:count]
 
-length = 2*count -1 
+#length = 2*count -1 
 
+#This is the middle line
+result = [alphabet[1::][::-1] + alphabet]
 
-word = 'A'
+#print(f'length: {length}')
+#print(f'lettters: {alphabet}')
+#print(f'middle line: {result}')
 
-for i in range(2,count+1):
-    #print(i-1)
-    word = alphabet[i-1] + word + alphabet[i-1]
+for i in range(0,count-1):
+    #print(f'i: {i}, i+1 = {i+1}')
+    new_line = result[0].replace(alphabet[i],alphabet[i+1])
+    result = [new_line] + result[:] + [new_line]
+    #print(new_line)
 
-matrix = [word]
-
-for i in
+#Now print the result from the list result[]
+for line in result:
+    print(line)
