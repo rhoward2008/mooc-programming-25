85c9c7b5236df98e3006175b4c6361a247333c59
Dworkin <bension.dworkin@baesystems.us>
2026-03-09 15:40:44 -0700

    WIP

--- part07-09_how_old/src/how_old.py
+++ part07-09_how_old/src/how_old.py
@@ -1,1 +1,20 @@
-# Write your solution here
+from datetime import datetime
+
+def age_diff():
+    day = int(input('Day: '))
+    month = int(input('Month: '))
+    year = int(input('Year: '))
+
+    date_born = datetime(year, month, day)
+    y2k_date = datetime(1999,12,31)
+    diff = y2k_date - date_born
+
+    if date_born > y2k_date:
+        print("You weren't born yet on the eve of the new millenium")
+    elif date_born == y2k_date:
+        print('You are a y2k baby!')
+    else:
+        print(f'You were {diff.days} days old on the eve of the new millenium')
+
+if __name__ == '__main__':
+    age_diff()
--- part07-10_valid_pic/src/valid_pic.py
+++ part07-10_valid_pic/src/valid_pic.py
@@ -1,1 +1,78 @@
-# Write your solution here
+from datetime import datetime
+import string
+
+def is_it_valid(pic: str) -> bool:
+    '''
+    Function to determine if pic is valid.  Must be in format ddmmyyXyyyz
+
+    Return True if valid, else False
+    '''
+
+    #First check length, must be 11
+    if len(pic) != 11:
+        return False
+    
+    #extract date of birth: day, month
+    try:
+        day = int(pic[0:2])
+        month = int(pic[2:4])
+        year = int(pic[4:6])
+    except ValueError:
+        return False
+    
+    #find dob year
+    century = pic[6]
+    if century == '+':
+        year = 1800 + year
+    elif century == '-':
+        year = 1900 + year
+    elif century == 'A':
+        year = 2000 + year
+    else: #Invalid pic, return False
+        return False
+    
+    #Put together dd, mm, yyyy for date of birth
+    try:
+        dob = datetime(year,month,day)
+    except ValueError:
+        return False
+    
+    #print(f'dob: {dob}')
+
+    id = pic[7:10]
+    #check that id is valid, must be 3 digits
+    for i in id:
+        if i not in string.digits:
+            return False
+    
+    control_char = pic[10]
+    dividend =  int(pic[0:2] + pic[2:4] + pic[4:6] + id)
+    #print(f'numerator: {dividend}')
+
+    #check that control is valid
+    remainder = dividend % 31
+
+    lookup = '0123456789ABCDEFHJKLMNPRSTUVWXY'
+    if control_char != lookup[remainder]:
+        return False
+
+    #print(f'control_char: {control_char}')
+    #print(f'lookup: {lookup[remainder]}')
+    #print(f'sum: {dividend}')
+    #print(f'remainder: {remainder}')
+    
+
+    return True
+
+if __name__ == '__main__':
+    #pic1 = '230827-906F'
+    #pic1 = '120488+246L'
+    #pic1 = '310823A9877'
+    pic1 = '310899-9877'
+
+    p1 = is_it_valid(pic1)
+
+    if p1:
+        print(f'{pic1} is valid')
+    else:
+        print(f'{pic1} not valid')
