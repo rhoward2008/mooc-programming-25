7bcd15b9b54ab303aad34b3d116402c95bad04f5
Dworkin <bension.dworkin@baesystems.us>
2026-03-12 16:56:59 -0700

    WIP

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,7 +1,22 @@
 import json
 
+PATH = 'c:\\Users\\bension.dworkin\\github_repo\\mooc-programming-25\\part07-12_json_files\\src\\'
+
+
 def print_person(filename: str):
-    pass
+    with open(filename) as file_handle:
+        file_data = file_handle.read()
+
+    person_list = json.loads(file_data)
+
+    
+    print(person_list)
+
+    for person in person_list:
+        print(f'{person['name']} {person['age']} years ',end='')
+        print('(' + ', '.join(person['hobbies']) + ')')
 
 if __name__ == '__main__':
-    pass
+    file_name = 'person.json'
+    full_file = PATH + file_name
+    print_person(full_file)
