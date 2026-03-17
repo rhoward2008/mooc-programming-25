9f787bbc861a747398af1c7f4c68825ee72f23a0
Dworkin <bension.dworkin@baesystems.us>
2026-03-17 14:34:20 -0700

    WIP

--- part07-14_who_cheated/src/who_cheated.py
+++ part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,12 @@
-# Write your solution here
+PATH = 'c:\\Users\\bension.dworkin\\github_repo\\mooc-programming-25\\part07-14_who_cheated\\src\\'
+
+start_file = PATH + 'start_times.csv'
+end_file = PATH + 'submissions.csv'
+
+def cheaters():
+    pass
+
+
+if __name__ == '__main__':
+    cheater_list = cheaters()
+    print(cheater_list)
