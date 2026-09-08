2a01328fef45c985228b57070f24f7d7229eda99
Dworkin <bension.dworkin@baesystems.us>
2026-09-08 11:36:59 -0700

    WIP

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,30 @@
-# Write your solution here
+from difflib import get_close_matches
+
+PATH = 'c:\\Users\\bension.dworkin\\github_repo\\mooc-programming-25\\part07-16_spellchecker_2\\src\\'
+file_name = 'wordlist.txt'
+words_file = PATH + file_name
+
+words_master = set()
+
+
+
+with open(words_file,'r') as dict_file:
+    for line in dict_file:
+        words_master.add(line.strip().lower())
+
+user_input = input('Write text: ')
+sentence = user_input.split(' ')  
+
+suggestions = []
+
+for word in sentence:
+    if word.lower() in words_master:
+        print(word, end=' ')
+    else :
+        print(f'*{word}* ', end='')
+        suggestions.append([[word],get_close_matches(word,words_master)])
+
+print('\nsuggestions:')
+
+for match in suggestions:
+    print(f'{match[0][0]}: {', '.join(match[1])}')
--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,37 @@
-# Write your solution here
+import string
+
+def change_case(orig_string: str) -> str:
+    change = ''
+    for char in orig_string:
+        if char in string.ascii_lowercase:
+            change += char.upper()
+        elif char in string.ascii_uppercase:
+            change += char.lower()
+
+    return change
+
+def split_in_half(orig_string: str) -> tuple:
+    half_way = int(len(orig_string)/2)
+    part1 = orig_string[0:half_way]
+    part2 = orig_string[half_way:]
+
+    return part1,part2
+
+def remove_special_characters(orig_string: str) -> str:
+    removed_char = ''
+
+    for char in orig_string:
+        if char in string.ascii_letters or char in string.ascii_uppercase or char in string.digits or char in string.whitespace:
+            removed_char += char
+
+    return removed_char
+
+if __name__ == '__main__':
+    p1 = change_case('AbCdE')
+    #print(p1)
+
+    p2 = split_in_half('abcdef')
+    #print(p2)
+
+    p3 = remove_special_characters('ab5% CD')
+    print(p3)
